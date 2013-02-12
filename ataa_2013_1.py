c6a4c2ad5972fef2cb40e247bf40e2d7bbe1de6f
Amogh Gudi <amogh.gudi@student.uva.nl>
2013-02-12 14:42:39 +0100

    End of commit testing. All issues sortof resolved.

--- ataa_2013_1.py
+++ ataa_2013_1.py
@@ -35,4 +35,3 @@ class Tournament1(run.Scenario):
 
 #Tournament1.one_on_one(red="domination/agent.py", blue="domination/agent.py", output_folder='_tmp')
 Tournament1.test(red="domination/agent.py", blue="domination/agent.py")
-#One more test 2.
