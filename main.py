60e8d671f41c475e7949d7e98d746c22fa27293c
fleischhauf <fleischhauf@hotmail.com>
2013-02-13 21:15:36 +0100

    added main.py to be able to disable renderer added first version of deterministic agend: 1 and 2 agent will try to capture and hold the ammo points 3 agent will run around the field without ammo and try to capture control points

---
+++ main.py
@@ -0,0 +1,46 @@
+from domination import core
+import math
+
+FIELD = """
+w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ w _ C _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ w _ _ _ w w w w w w w w w w w _ _ _ _ _ _ w
+w _ _ w _ _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ w
+w R _ w _ _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ B w
+w R _ w _ _ _ w _ A _ _ w w w w w _ _ A _ w _ _ _ w _ B w
+w R _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ _ w _ B w
+w _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ _ w _ _ w
+w _ _ _ _ _ _ w w w w w w w w w w w _ _ _ w _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ C _ w _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
+w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
+"""
+
+'''
+if(self.id == 0):
+    self.goal = (152,136)
+'''
+'''            
+if(self.id == 0):
+    left ammo = (152,136)
+if(self.id == 1):
+    right ammo  = (312,136)
+    ctr bot #(248, 216)
+    ctr top #(220, 56)
+'''
+# Make it a short game
+#settings = core.Settings(max_steps=1000)
+settings = core.Settings(max_steps=10000,ammo_amount=2,spawn_time=10,ammo_rate=14,max_see=70,max_turn=math.pi/4,max_score=100000)
+FI = core.Field.from_string(FIELD)
+# Initialize a game
+#game = core.Game('domination/agent.py', 'domination/functiontest.py',
+game = core.Game('domination/agent.py', 'domination/deterministic_agents.py',
+    record=True, rendered=True, settings=settings,field=FI)
+#ammo_amount=2,spawn_time=10,ammo_rate=14,max_see=70,max_turn=math.pi/4,max_score=100
+# Will run the entire game.
+game.run()
