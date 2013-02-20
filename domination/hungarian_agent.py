14c98ab1b1adb301a7ec138bd71f2cda693de360
johndzervos <johndzervos@gmail.com>
2013-02-20 21:52:04 +0100

    hungarian agent

--- domination/hungarian_agent.py
+++ domination/hungarian_agent.py
@@ -222,8 +222,7 @@ class Agent(object):
             print self.all_agents[2].observation.loc
             print self.get_path_length(self.all_agents[2].observation.loc,self.path1)
             print self.costMatrix
-            #print self.costMatrix[1][1]
-            ##
+            ##print self.costMatrix[1][1]
             
             m = Munkres()
             indexes = m.compute(self.costMatrix)
