1cf12d15319cdf58172401f4778bfacdda13b581
fleischhauf <fleischhauf@hotmail.com>
2013-03-03 21:59:26 +0100

    made minor changes

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
 w _ _ _ _ _ _ _ w w w w w w w w w w w w w w w _ _ _ _ _ _ _ w
 w _ _ _ w _ _ _ w _ _ _ _ _ _ _ _ _ _ a _ _ _ _ _ _ w _ _ _ w
 w R _ _ w _ _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ B w
-w _ _ _ w _ _ _ w _ _ _ _ w w w w w _ _ _ _ w _ _ _ w _ _ B w
-w _ _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ _ w _ _ B w
+w R _ _ w _ _ _ w _ _ _ _ w w w w w _ _ _ _ w _ _ _ w _ _ B w
+w R _ _ w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w _ _ _ w _ _ B w
 w _ _ _ w _ _ _ _ _ _ a _ _ _ _ _ _ _ _ _ _ w _ _ _ w _ _ _ w
 w _ _ _ _ _ _ _ w w w w w w w w w w w w w w w _ _ _ _ _ _ _ w
 w _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ w
@@ -40,7 +40,8 @@ w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w w
 """
 
 #These are the oficial settings
-settings = core.Settings(max_turn=0.78539816339744828, think_time=0.059999999999999998, ammo_rate=9, spawn_time=11, ammo_amount=1, max_steps=20000, max_score=100000000, max_see=80)
+#settings = core.Settings(max_turn=0, think_time=0.059999999999999998, ammo_rate=9, spawn_time=11, ammo_amount=1, max_steps=300, max_score=100, max_see=80)
+settings = core.Settings(max_turn=0.78539816339744828, think_time=0.059999999999999998, ammo_rate=9, spawn_time=11, ammo_amount=1, max_steps=300, max_score=100, max_see=80)
 FI = core.Field.from_string(FIELD)
 score = 0
 ammo = 0
@@ -49,21 +50,35 @@ kills = 0
 deaths = 0
 thinktime = 0
 steps = 0
-
-for i in range(1, 2):
+steps_mov = [0,0,0,0,0,0,0,0,0,0]
+for i in range(0, 1000000):
+    
     #red_init={'blob': open('stateActionValues.p','rb')}
-    game = core.Game('domination/wolf3.py', 'domination/defensive_agent.py',red_init={'blob': open('stateActionValues.p','rb')},
-                     record=True, rendered=True,verbose=True, settings=settings, field=FI)
+    if(i%20 == 0):
+        game = core.Game('domination/q_learning_reduced_space.py', 'domination/agent.py',red_init={'blob': open('stateActionValues-reduced.p','rb')},
+                     record=False, rendered=True,verbose=True, settings=settings, field=FI)
+    else:
+        game = core.Game('domination/q_learning_reduced_space.py', 'domination/sillybot.py',red_init={'blob': open('stateActionValues-reduced.p','rb')},
+                     record=False, rendered=False,verbose=False, settings=settings, field=FI)
     game.run()
     #print game.stats
-    print "Game:", i, "| Score:", game.stats.score_blue
-    score = score + game.stats.score_blue
-    ammo += game.stats.ammo_blue
-    enemyammo += game.stats.ammo_red
-    kills += game.stats.deaths_red
-    deaths += game.stats.deaths_blue
-    thinktime += game.stats.think_time_blue
-    steps += game.stats.steps
+
+    steps_mov.pop(0)
+    steps_mov.append(game.stats.steps)    
+    steps_total = 0
+    for j in steps_mov:
+        steps_total += j
+    steps_avrg = steps_total/len(steps_mov)
+    print "Game:", i, "| Score:", game.stats.score_red,"| steps:",game.stats.steps,"| avrg_steps:",steps_avrg
+    #if(game.stats.score_red > 90):
+    #    break
+    #score = score + game.stats.score_blue
+    #ammo += game.stats.ammo_blue
+    #enemyammo += game.stats.ammo_red
+    #kills += game.stats.deaths_red
+    #deaths += game.stats.deaths_blue
+    #thinktime += game.stats.think_time_blue
+    #steps += game.stats.steps
 print "Win Ratio:", score/i, "%"
 print "Average Kills:", kills/i
 print "Average Deaths:", deaths/i
@@ -71,3 +86,4 @@ print "Average Ammo:", ammo/i
 print "Average Enemy Ammo:", enemyammo/i
 print "Average Think Time:", thinktime/i
 print "Average Game Steps:", steps/i
+
